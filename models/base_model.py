c6dfea81e8832f3700e16d85152f2d9635a48ecf
larini abdellah <laribox2014@gmail.com>
2024-06-26 05:06:38 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,9 +17,8 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
+        if not kwargs:            
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
