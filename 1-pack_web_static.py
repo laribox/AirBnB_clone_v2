8d22683e1f592b9e037625640cca8b06cf7422b7
laribox <laribox2014@gmail.com>
2024-07-11 10:46:03 -0700

    task 1 answer

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Generates a .tgz archive from the contents of the web_static
+"""
+
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    """
+    create an archive from web_static dir
+    """
+    time = datetime.now()
+    archive_name = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+
+    result = local('tar -cvzf versions/{} web_static'.format(archive_name))
+    if result is not None:
+        return archive_name
+    else:
+        return None
