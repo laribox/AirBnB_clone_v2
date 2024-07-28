55ee006e0efa1cae4023e9883c9e686fbabc45d0
laribox <laribox2014@gmail.com>
2024-07-28 00:24:03 +0000

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,15 +11,24 @@ def hello():
     """Return a given string"""
     return ("Hello HBNB!")
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return a given string"""
     return ("HBNB")
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Return a given string"""
-    return (f"C {text}")
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Return a given string"""
+    return 'C {}'.format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
