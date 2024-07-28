a76c617a5617398ac4d8cbf4fd1ce0fa710d1b12
laribox <laribox2014@gmail.com>
2024-07-28 00:31:52 +0000

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return a given string"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Return a given string"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text = "is cool"):
+    """Return a given string"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    """display n is a number only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
